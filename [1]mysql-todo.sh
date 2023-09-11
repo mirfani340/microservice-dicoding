b30a65224ad2862849787d4eb3fa68f170d3e1c4
mirfani340 <muhammadirfani340@gmail.com>
2023-09-11 09:12:40 +0000

    update code

--- [1]mysql-todo.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-docker run -d \
-  --name mysql-db \
-  --network todo-app --network-alias mysql \
-  -v todo-mysql-data:/var/lib/mysql \
-  -e MYSQL_ROOT_PASSWORD=dicoding \
-  -e MYSQL_DATABASE=todo-db \
-  mysql:5.7
-
